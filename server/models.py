ce58980564b960dd8f97d711a995cf7c35b577c6
CatM <catmorillo@hotmail.com>
2023-04-17 10:53:05 -0400

    models

--- server/models.py
+++ server/models.py
@@ -7,5 +7,44 @@ from sqlalchemy_serializer import SerializerMixin
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
-
 db = SQLAlchemy(metadata=metadata)
+
+class Attack(db.Model, SerializerMixin):
+    __tablename__ = "attacks"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    location = db.Column(db.String)
+    date = db.Column(db.String)
+    created_at = db.Column(db.DateTime,server_default=db.func.now())
+    updated_at = db.Column(db.DateTime,onupdate=db.func.now())
+    pirates = db.relationship("Pirate", backref="attack")
+
+    ships = association_proxy("pirates", "ship")
+
+class Ship(db.Model, SerializerMixin):
+    __tablename__ = "ships"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    size = db.Column(db.String)
+    created_at = db.Column(db.DateTime,server_default=db.func.now())
+    updated_at = db.Column(db.DateTime,onupdate=db.func.now())
+    pirates = db.relationship("Pirate", backref="ship")
+
+    attacks = association_proxy("pirates", "attack")
+
+class Pirate(db.Model, SerializerMixin):
+    __tablename__= "pirates"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    age = db.Column(db.Integer)
+    attack_id = db.Column(db.Integer, db.ForeignKey ("attacks.id"))
+    ship_id = db.Column(db.Integer, db.ForeignKey ("ships.id")) 
+
+
+
+
+
+
+
+
+
