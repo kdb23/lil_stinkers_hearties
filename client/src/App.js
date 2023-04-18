e43b8e169734806dd4c047f8374a88ebd14b2854
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-04-18 09:13:34 -0400

    adding of initial routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 import {useEffect, useState} from 'react'
+import Home from './Home'
+import {Route, Switch} from 'react-router-dom'
 import ShipContainer from './ShipContainer'
 import AttackContainer from './AttackContainer'
 import PiratesContainer from './PiratesContainer'
+
 
 function App() {
 
@@ -33,9 +36,23 @@ function App() {
 
   return (
     <div>
-      <ShipContainer ships={ships} />
-      <AttackContainer attacks={attacks} />
-      <PiratesContainer pirates={pirates} />
+      <Switch>
+        <Route exact path = '/'>
+          <Home />
+        </Route>
+        <Route exact path = '/ships'>
+          <ShipContainer ships={ships} />
+        </Route>
+        {/* <Route exact path = '/ships/:id'>
+            <ShipDetails />
+        </Route> */}
+        <Route exact path = '/attacks'>
+          <AttackContainer attacks={attacks} />
+        </Route>
+        <Route exact path = '/pirates'>
+          <PiratesContainer pirates={pirates} />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ client/src/Home.js
@@ -0,0 +1,8 @@
+
+function Home() {
+    return  ( <h1> 🧜‍♀️HOME 🧜‍♀️</h1>
+    )
+
+}
+
+export default Home
