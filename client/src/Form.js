df2ddb3dfc159f5b5d2e71d87d58a6263fafa0cf
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-04-19 12:25:16 -0400

    addition of css tailwind and starter code for forms

--- client/src/Form.js
+++ client/src/Form.js
@@ -146,3 +146,57 @@ function Form({addAttack, addShip, addPirate}) {
 
 }
 export default Form 
+
+// // <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
+// <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-rose-600/40 ring-2 ring-indigo-600 lg:max-w-xl">
+// <h1 className="text-3xl font-semibold text-center text-indigo-700 uppercase">
+//     Join A Ship
+// </h1>
+// <form className="mt-6">
+//     <div className="mb-2">
+//         <label
+//             for="text"
+//             className="block text-sm font-semibold text-gray-800"
+//         >
+//             Ship Name
+//         </label>
+//         <input
+//             type="name"
+//             className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
+//         />
+//     </div>
+//     <button id='dropdownDefaultButton' data-dropdown-toggle='dropdown' class='text-white bg-purple-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium round-le text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-700 dark:focus:ring-blue-800' type='button'> Select Ship Size  <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
+//     <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+//     </div>
+//     {/* <div className="mb-2">
+//         <label
+//             for="name"
+//             className="block text-sm font-semibold text-gray-800"
+//         >
+//             Ship Size
+//         </label>
+//         <input
+//             type=""
+//             className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
+//         />
+//     </div> */}
+//     <div className="mb-2">
+//         <label
+//             for="email"
+//             className="block text-sm font-semibold text-gray-800"
+//         >
+//             Email
+//         </label>
+//         <input
+//             type="email"
+//             className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
+//         />
+//     </div>
+//     <div className="mt-6">
+//         <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-indigo-700 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600">
+//             🏴‍☠️🌊🧜‍♀️☠️
+//         </button>
+//     </div>
+// </form>
+// </div>
+// </div>
--- client/src/Pirates.js
+++ client/src/Pirates.js
@@ -70,3 +70,64 @@ function Pirates({name, age, rank, id, handlePirateDelete, handlePiratePatch}){
 
 
 export default Pirates
+
+// function ModalForm({isVisible, handleClose}) {
+
+//     // const [visible, setVisible] = useState(isVisible);
+
+//     // const toggleModal = () => {
+//     //     setVisible(!visible);
+//     // };
+
+//   return (
+//     <div>
+
+//       {isVisible && (
+//         <div className="fixed z-10 inset-0 overflow-y-auto">
+//           <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+//             <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+//               <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+//             </div>
+//             <div
+//               className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+//               <div>
+//                 <div className="mt-3 text-center sm:mt-5">
+//                   <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
+//                     Click the Flag to Make the Pirate Walk The Plank
+//                   </h3>
+//                   <div className="mt-2">
+//                     <p className="text-sm text-gray-500">
+//                       🦈🌊🏴‍☠️☠️
+//                     </p>
+//                   </div>
+//                 </div>
+//               </div>
+//               <div className="mt-5 sm:mt-6">
+//                 <button type="button"
+//                   className= "animate-pulse inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-black text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black-500 sm:text-sm"
+//                   onClick={handleClose}
+//                 >
+//                 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//                 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣾⣿⣿⣿⣿⣷⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//                 ⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//                 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//                 ⠀⣄⠀⠀⠀⠀⠀⠀⠈⢿⡇⠀⠈⣿⣿⠁⠀⢸⡿⠁⠀⠀⠀⠀⠀⠀⣠⠀⠀
+//                 ⠀⠀⢹⣦⠀⠀⠀⠀⠀⠀⠻⣷⣤⣴⢏⡹⣦⣤⣾⠟⠀⠀⠀⠀⠀⠀⣴⡏⠀⠀
+//                 ⠀⠀⠀⢿⣷⣄⠀⠀⠀⠀⠀⠀⠙⠛⠛⠛⠛⠋⠀⠀⠀⠀⠀⠀⣠⣾⡿⠀⠀⠀
+//                 ⠀⠀⠀⠈⢿⣿⣧⡀⠀⠀⠀⢸⣧⡀⠀⠀⢀⣼⡇⠀⠀⠀⢀⣼⣿⡿⠁⠀⠀⠀
+//                 ⠀⠀⠀⠀⠈⠻⣿⣿⣦⣀⠀⠀⠙⠿⣷⣾⠿⠋⠀⠀⣀⣴⣿⣿⠟⠁⠀⠀⠀⠀
+//                 ⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣷⣄⡀⠀⠀⠀⠀⢀⣠⣾⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀
+//                 ⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⢿⣿⠿⠒⢀⣠⣶⣿⣿⡿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀
+//                 ⠀⠀⠀⠀⠀⠀⠀⠰⢶⡄⠀⣠⣴⣾⣿⣿⡿⠟⠋⠀⢠⡶⠆⠀⠀⠀⠀⠀⠀⠀
+//                 ⠀⠀⠀⠀⣀⠀⣀⣤⡈⢻⣆⠸⠿⠟⠋⠁⠠⠶⠇⣰⡟⢁⣤⣀⠀⣀⠀⠀⠀⠀
+//                 ⠀⠀⠀⢸⣿⡇⠘⠛⠁⠀⠻⠆⠀⠀⠀⠀⠀⠀⠰⠟⠀⠈⠛⠃⢸⣿⡇⠀⠀⠀
+//                 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//                 </button>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
