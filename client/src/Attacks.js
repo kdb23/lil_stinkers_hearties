11ab9f944973bd730c3ab1d3ac54b5f2b39c3b71
Raymond An <raymond_an@yahoo.com>
2023-04-20 16:08:00 -0400

    css for attacks and ships

--- client/src/Attacks.js
+++ client/src/Attacks.js
@@ -3,10 +3,18 @@ import React from 'react';
 function Attacks({name,location, date}){
 
     return(
-        <div>
-            <h1>{name}</h1>
-            <p>{location}</p>
-            <p>{date}</p>
+        <div className='border-solid border-2 border-grey-400 px-5 ph-5 animate-pulse'>
+            <h1 className='mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50'>
+                {name}
+            </h1>
+
+            <p className='mb-4 text-base text-neutral-600 dark:text-neutral-200'>
+                Location: {location}
+            </p>
+
+            <p className='text-xs text-neutral-500 dark:text-neutral-300'>
+                Occured: {date}
+            </p>
         </div>
     )
 }
--- client/src/Ships.js
+++ client/src/Ships.js
@@ -3,11 +3,25 @@ import React from 'react';
 function Ships({name,size,pirates,battles}){
 
     return(
-        <div>
-            <h1>{name}</h1>
-            <p>{size}</p>
-           <p>{pirates}</p>
-           <p>{battles}</p>
+        <div className='border-solid border-2 border-grey-400 px-5 ph-5'>
+            <h1 className='mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50'>
+                The {name}
+            </h1>
+
+            <p className='text-s text-neutral-500 dark:text-neutral-300'>
+                Size: {size}
+            </p>
+            <div className='grid grid-cols-2 gap-2'>
+                <div>
+                    <h2 className='mb-4 text-xl text-neutral-600 dark:text-neutral-200 pt-2'>Lil Stinkers 🦨</h2>
+                    {pirates}
+                </div>
+
+                <div>
+                    <h2 className='mb-4 text-xl text-neutral-600 dark:text-neutral-200 pt-2'>Battles 💣</h2>
+                    {battles}
+                </div>
+            </div>
         </div>
     )
 }
