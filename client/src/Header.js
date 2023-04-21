2b552f6eeaf24c2f5be544506d1b17443cd4ff2e
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-04-20 22:38:37 -0400

    changes to flag

--- client/src/Header.js
+++ client/src/Header.js
@@ -6,12 +6,12 @@ function Header() {
     return(
 
         <div>
+            <div className='animate-waving-flag justify-center leading-non flex'>
+                <img src='./images/lilstinkersflag.png' />
             <div className= 'flex items-right'>
                 <audio controls className='outline-none h-10 w-48' loop>
                     <source src={AudioFile} type='audio/mpeg' />
                 </audio>
-            <div className='animate-waving-flag leading-non flex'>
-                <img src='./images/lilstinkersflag.png' />
             </div>
             </div>
         </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,17 +8,17 @@ module.exports = {
       keyframes: {
         wave: {
           '0%': { transform: 'rotate(0.0deg)' },
-          '10%': { transform: 'rotate(14deg)' },
-          '20%': { transform: 'rotate(-8deg)' },
-          '30%': { transform: 'rotate(14deg)' },
-          '40%': { transform: 'rotate(-4deg)' },
-          '50%': { transform: 'rotate(10.0deg)' },
+          '10%': { transform: 'rotate(-4deg)' },
+          '20%': { transform: 'rotate(0deg)' },
+          '30%': { transform: 'rotate(-4deg)' },
+          '40%': { transform: 'rotate(0deg)' },
+          '50%': { transform: 'rotate(-4.0deg)' },
           '60%': { transform: 'rotate(0.0deg)' },
-          '100%': { transform: 'rotate(0.0deg)' },
+          '100%': { transform: 'rotate(0deg)' },
         },
       },
       animation: {
-        'waving-flag': 'wave 100s linear infinite',
+        'waving-flag': 'wave 10s linear infinite',
       }
     },
   },
